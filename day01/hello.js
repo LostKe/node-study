6b6aafe9682af678043434656c5ce7ee8c438889
shuqing.me@gmail.com <shuqing.me@gmail.com>
2017-11-09 15:24:32 +0800

    update

--- day01/hello.js
+++ day01/hello.js
@@ -8,6 +8,11 @@ function Hello(self){
   this.sayHello=function(target){
       console.log("%s 对 %s 说hello,%s",self,target,location);
   };
+
+  this.excute=function(invoke){
+    var result=invoke+3;
+    console.log("结果是%d,",result);
+  }
 }
 
 module.exports=Hello;
--- day01/testModule.js
+++ day01/testModule.js
@@ -4,3 +4,11 @@ var Hello=require("./hello");
 var hello=new Hello("主任");
 hello.setLocation("北京");
 hello.sayHello("小明");
+
+
+function add(x,y){
+    return x+y;
+}
+
+//传递一个函数
+hello.excute(add(5,5));
